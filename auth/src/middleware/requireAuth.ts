a1c13baaef8e26c08a158e4dbbcc8f3bc948db66
Destiny Ross <destinylaross@gmail.com>
2022-01-09 09:34:33 -0500

    finished auth service logic, adding a NotAuthorizedError, and middlewares that adds current user to the req.currentUser property and block access to unauthenticated users

---
+++ auth/src/middleware/requireAuth.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from "express";
+import { NotAuthorizedError } from "../errors/not-authorized-error";
+
+export const requireAuth = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  //require auth will only run after currentuser middleware
+
+  if (!req.currentUser) {
+    throw new NotAuthorizedError();
+  }
+
+  next();
+};
