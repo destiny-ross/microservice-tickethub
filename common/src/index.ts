9e539b789711f7f56bfcd130f5974e9aec61e4c0
Destiny Ross <destinylaross@gmail.com>
2022-01-15 20:19:19 -0500

    refactored errors and middleware to common module for shared consumption

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,15 +1,10 @@
-interface Color {
-  red: number;
-  blue: number;
-  green: number;
-}
+export * from "./errors/bad-request-error";
+export * from "./errors/custom-error";
+export * from "./errors/database-connection-error";
+export * from "./errors/not-authorized-error";
+export * from "./errors/not-found-error";
+export * from "./errors/request-validation-error";
 
-const color: Color = {
-  red: 10,
-  blue: 10,
-  green: 10,
-};
-
-console.log(color);
-
-export default color;
+export * from "./middleware/currentUser";
+export * from "./middleware/errorHandler";
+export * from "./middleware/requireAuth";
