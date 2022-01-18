e486900de889e52fd7de0fc8c87cad2aa59694a4
Destiny Ross <destinylaross@gmail.com>
2022-01-18 05:30:58 -0500

    added ticket service

---
+++ tickets/src/app.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+import { NotFoundError, errorHandler } from "@dross-tickets/common";
+
+const app = express();
+
+// global middleware
+app.set("trust proxy", true);
+app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+
+// routers
+
+// Not found
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
