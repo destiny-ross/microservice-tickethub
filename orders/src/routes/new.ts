2239d2c0da2375be19653d239ed6278fc62989e2
Destiny Ross <destinylaross@gmail.com>
2022-01-28 05:09:19 -0500

    created order and ticket models and built controller for order creation

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,8 +1,18 @@
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
-import { requireAuth, validateRequest } from "@dross-tickets/common";
+import {
+  NotFoundError,
+  requireAuth,
+  validateRequest,
+  OrderStatus,
+  BadRequestError,
+} from "@dross-tickets/common";
 import { body } from "express-validator";
+import { Ticket } from "../models/ticket";
+import { Order } from "../models/order";
 const router = express.Router();
+
+const EXPIRATION_WINDOW_SECONDS = 16 * 60;
 
 router.post(
   "/api/orders",
@@ -18,7 +28,35 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
-    res.send({});
+    const { ticketId } = req.body;
+    // find ticket user attempting to order
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    // check for existing reservation
+    const isReserved = await ticket.isReserved();
+    if (isReserved) {
+      throw new BadRequestError("Ticket is already reserved");
+    }
+
+    // calc expiration date for order (15 minutes)
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+    // build order, save it
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket,
+    });
+
+    await order.save();
+
+    // publish event
+    res.status(201).send(order);
   }
 );
 
