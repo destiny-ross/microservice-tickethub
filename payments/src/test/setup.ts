5d3486c67cd17964e45bd3bb8aaf0df92b542b73
Destiny Ross <destinylaross@gmail.com>
2022-02-12 05:09:19 -0500

    payments service bootstrap

---
+++ payments/src/test/setup.ts
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import jwt from "jsonwebtoken";
+
+let mongo: any;
+
+declare global {
+  var signin: () => string[];
+}
+
+//hook that runs before all tests are executed
+process.env.JWT_KEY = "jewgbkjewbmn,wb";
+jest.mock("../nats-wrapper");
+beforeAll(async () => {
+  mongo = await MongoMemoryServer.create();
+  const mongoURI = await mongo.getUri();
+  await mongoose.connect(mongoURI);
+});
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
+
+global.signin = () => {
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: "test@test.com",
+  };
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+  const session = { jwt: token };
+  const sessionJSON = JSON.stringify(session);
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+  return [`session=${base64}`];
+};
